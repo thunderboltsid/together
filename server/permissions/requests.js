6da4d27241323cbe881340a52ffdf49cc945bf4d
siddharthshukla <s.shukla@jacobs-university.de>
2016-05-23 11:38:26 +0300

    add permissions

---
+++ server/permissions/requests.js
@@ -0,0 +1,11 @@
+Requests.allow({
+  'insert': function(userId, doc) {
+    return userId;
+  },
+  'update': function(userId, doc, fields, modifier) {
+    return userId;
+  },
+  'remove': function(userId, doc) {
+    return userId;
+  }
+});
